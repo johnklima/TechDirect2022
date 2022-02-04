bb2263c83b39320dd4b44789bb95a59be3b1376f
TomIngve <97891438+TomIngve@users.noreply.github.com>
2022-02-04 21:22:15 +0100

    Updated footstep script, added some terrain type Tags
    Checks if you're close to the ground and the tag of the collider below.

--- Assets/Scripts/Minimum/FootstepsPlayer/FootstepsPlayer.cs
+++ Assets/Scripts/Minimum/FootstepsPlayer/FootstepsPlayer.cs
@@ -3,12 +3,16 @@ using UnityEngine;
 public class FootstepsPlayer : MonoBehaviour
 {
     public float speed = 5;
-    Vector2 velocity;
     public AudioSource source;
-    public AudioClip footstepLeft;
-    public AudioClip footstepRight;
+    public AudioClip footstepLeftWood;
+    public AudioClip footstepRightWood;
+    public AudioClip footstepLeftBox;
+    public AudioClip footstepRightBox;
+    public AudioClip footstepLeftBed;
+    public AudioClip footstepRightBed;
     private float stepRate;
     private bool leftFoot;
+
 
     private void Start()
     {
@@ -18,31 +22,74 @@ public class FootstepsPlayer : MonoBehaviour
     void FixedUpdate()
     {
 
-        //if ((velocity.y != 0) || (velocity.x != 0))
-        if ((Input.GetAxis("Horizontal")!=0) || (Input.GetAxis("Vertical") != 0))
-        {
-            stepRate += 1;
-        }
-
-        if (stepRate == 6)
         {
 
-            if (leftFoot == true)
+            if ((Input.GetAxis("Horizontal") != 0) || (Input.GetAxis("Vertical") != 0))
             {
-                Debug.Log("Left");
-                source.clip = footstepLeft;
-                source.Play();
-                stepRate = 0;
-                leftFoot = false;
+                
+                stepRate += 1;
             }
 
-            else
+            if (stepRate >= 7)
             {
-                Debug.Log("right");
-                source.clip = footstepRight;
-                source.Play();
-                stepRate = 0;
-                leftFoot = true;
+                if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit terrain, 0.1f + 0.1f))
+                {
+                    if (leftFoot == true)
+                    {
+                        if (terrain.transform.CompareTag("Wood"))
+                        {
+                            //Debug.Log("wood hit");
+                            source.clip = footstepLeftWood;
+
+                        }
+
+                        if (terrain.transform.CompareTag("Box"))
+                        {
+                            //Debug.Log("box hit");
+                            source.clip = footstepLeftBox;
+
+                        }
+
+                        if (terrain.transform.CompareTag("Bed"))
+                        {
+                            //Debug.Log("bed hit");
+                            source.clip = footstepLeftBed;
+
+                        }
+
+                        source.Play();
+                        stepRate = 0;
+                        leftFoot = false;
+
+                    }
+
+                    else
+                    {
+
+                        if (terrain.transform.CompareTag("Wood"))
+                        {
+                            //Debug.Log("wood hit");
+                            source.clip = footstepRightWood;
+                        }
+
+                        if (terrain.transform.CompareTag("Box"))
+                        {
+                            //Debug.Log("box hit");
+                            source.clip = footstepRightBox;
+                        }
+
+                        if (terrain.transform.CompareTag("Bed"))
+                        {
+                            //Debug.Log("bed hit");
+                            source.clip = footstepRightBed;
+                        }
+
+                        source.Play();
+                        stepRate = 0;
+                        leftFoot = true;
+
+                    }
+                }
             }
         }
     }
